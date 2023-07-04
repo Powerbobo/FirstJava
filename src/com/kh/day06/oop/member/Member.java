9c18cb0d3c3a96a01fb1e9a2d6882965c52d58b5
JoJo <powerbobo88@gmail.com>
2023-07-04 17:16:41 +0900

    [INIT] 23/07/04 회원관리 프로그램
    1. 회원관리 프로그램
    - 클래스 파일 2개로 정리

--- src/com/kh/day06/oop/member/Member.java
+++ src/com/kh/day06/oop/member/Member.java
@@ -1,6 +1,47 @@
 package com.kh.day06.oop.member;
 
 public class Member {
-	// 입력받을거 3개정도 만들고
-	// 캡슐화의 원칠 적용해서 구성
+	// 멤버 변수 - 멤버 변수 접근제어자 private으로 설정! (정보 접근이 제한됨)
+	// 멤버 변수 -> 이름, 1차 점수, 2차 점수
+	private String name;
+	private int firstScore;
+	private int secondScore;
+	
+	// 기본 생성자 -> 클래스명과 동일하게 만들어야 한다!
+	public Member() {}
+	
+	// 매개변수 생성자
+	public Member(String name, int firstScore, int secondScore) {
+		this.name = name;
+		this.firstScore = firstScore;
+		this.secondScore = secondScore;
+	}
+	
+	// 메소드
+	public double getAvg() {
+		double avg = (double)(this.firstScore + this.secondScore)/2;
+		return avg;
+	}
+	
+	// getter 메소드
+	public String getName() {
+		return this.name;
+	}
+	public int getFirstScore() {
+		return this.firstScore;
+	}
+	public int getSecondScore() {
+		return this.secondScore;
+	}
+	
+	//setter 메소드
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setFirstScore(int firstScore) {
+		this.firstScore = firstScore;
+	}
+	public void setSecondScore(int secondScore) {
+		this.secondScore = secondScore;
+	}
 }
--- src/com/kh/day06/oop/member/MemberRun.java
+++ src/com/kh/day06/oop/member/MemberRun.java
@@ -1,14 +1,16 @@
 package com.kh.day06.oop.member;
 
+import java.util.Scanner;
+
 public class MemberRun {
 	public static void main(String [] args) {
 		Member [] memArrs = new Member[2];
 		finish :
 		while(true) {
 			int chioce = printMenu();
-			switch (chioce) {
+			switch(chioce) {
 			case 1 : 
-				inputScore(memArrs);
+				intputScore(memArrs);
 				break;
 			case 2 : 
 				printScore(memArrs);
@@ -19,51 +21,87 @@ public class MemberRun {
 			case 4 : 
 				showGoodBye();
 				break finish;
-			default : printException();
+			default : 
+				printException();
+				break;
 			}
 		}
 	}
-
-	private static int printMenu() {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	public static int printMenu() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("=== 학생 성적 확인 프로그램 ===");
+		System.out.println("1. 학생 성적 입력 : ");
+		System.out.println("2. 학생 성적 출력 : ");
+		System.out.println("3. 재평가 여부 확인");
+		System.out.println("4. 프로그램 종료");
+		System.out.print("메뉴 입력 : ");
+		int chioce = sc.nextInt();
+		return chioce;
 	}
-
-	private static void printException() {
-		// TODO Auto-generated method stub
-		
+	public static void intputScore(Member [] memArrs) {
+		for(int i = 0; i < memArrs.length; i++) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("====== 학생 성적 입력 ======");
+			System.out.println((i+1) + "번째 학생 정보 입력");
+			System.out.print("학생 이름 입력 : ");
+			String name = sc.next();
+			System.out.print("1차 점수 입력 : ");
+			int firstScore = sc.nextInt();
+			System.out.print("2차 점수 입력 : ");
+			int secondScore = sc.nextInt();
+			memArrs[i] = new Member();
+			memArrs[i].setName(name);
+			memArrs[i].setFirstScore(firstScore);
+			memArrs[i].setSecondScore(secondScore);
+		}
+	}
+	public static void printScore(Member [] memArrs) {
+		for(int i = 0; i < memArrs.length; i++) {
+			// 이 부분 출력이 안됨!
+			System.out.println((i+1) + "번째 학생 정보 출력");
+			System.out.println(memArrs[i].getName() + " 학생의  점수는 1차점수 : " + memArrs[i].getFirstScore() + 
+					"점 , 2차 점수 : " + memArrs[i].getSecondScore() + "점 입니다.");
+		}
+	}
+	public static void checkPass(Member [] memArrs) {
+		for(int i = 0; i < memArrs.length; i++) {
+			double avg = memArrs[i].getAvg();
+			int num1 = memArrs[i].getFirstScore();
+			int num2 = memArrs[i].getSecondScore();
+			System.out.println((i+1) + "번째, " + memArrs[i].getName() + "학생 확인 결과");
+			if(avg >= 60) {
+				if(num1 < 40) {
+					System.out.println("1차 시험 재시험 대상자입니다.");
+				} else if(num2 < 40) {
+					System.out.println("2차 시험 재시험 대상자입니다.");
+				} else {
+					System.out.println("통과하셨습니다.");
+				}
+			} else {
+				if(num1 < 60) {
+					System.out.println("1차 시험 재시험 대상자입니다.");
+				}
+				if(num2 < 60) {
+					System.out.println("2차 시험 재시험 대상자입니다.");
+				}
+			}
+		}
+	}
+	public static void showGoodBye() {
+		System.out.println("프로그램이 종료되었습니다.");
+	}
+	public static void printException() {
+		System.out.println("잘못 선택하셨습니다. 메뉴를 선택해주세요.");
 	}
 	
-	private static void showGoodBye() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private static void checkPass(Member[] memArrs) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private static void printScore(Member[] memArrs) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private static void inputScore(Member[] memArrs) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private static void printException(Member [] memArrs) {
-		// TODO Auto-generated method stub
-		
-	}
 	
-	private static int printMenu(Member [] memArrs) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-
-
+	
+	
+	
 }
+
+
+
+
+
